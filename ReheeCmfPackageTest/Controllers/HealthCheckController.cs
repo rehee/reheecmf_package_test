5b354af7a229f2ee773f388614b9919d5f845606
rehee <x3iii131@gmail.com>
2022-05-04 19:00:54 +0100

    update version add health check

---
+++ ReheeCmfPackageTest/Controllers/HealthCheckController.cs
@@ -0,0 +1,20 @@
+﻿using Cruds;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReheeCmfPackageTest.Controllers
+{
+  public class HealthCheckController : Controller
+  {
+    private readonly IContextFactory factory;
+
+    public HealthCheckController(IContextFactory factory)
+    {
+      this.factory = factory;
+    }
+    public IActionResult Index()
+    {
+      var result = factory.HealthCheckFunc();
+      return Ok(result.Content);
+    }
+  }
+}
---
+++ ReheeCmfPackageTest/Migrations/20220504175615_add-health-check.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReheeCmfPackageTest.Migrations
+{
+    public partial class addhealthcheck : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HealthChecks",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CheckDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsHealthy = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HealthChecks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HealthChecks");
+        }
+    }
+}
