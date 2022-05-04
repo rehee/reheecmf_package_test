950b65720e56b7221195ce3ac5d8fda9452c3308
rehee <x3iii131@gmail.com>
2022-05-04 16:09:46 +0100

    update package

--- ReheeCmfPackageTest/Controllers/HomeController.cs
+++ ReheeCmfPackageTest/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using Authenticates;
+using Cruds;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
+using ReheeCmfPackageTest.Entities;
 using ReheeCmfPackageTest.Models;
 using Requests;
 using System.Diagnostics;
@@ -11,14 +13,16 @@ namespace ReheeCmfPackageTest.Controllers
   public class HomeController : Controller
   {
     private readonly ILogger<HomeController> _logger;
+    private readonly IContext db;
     private readonly UserManagementOption option;
 
 
-    public HomeController(ILogger<HomeController> logger, IOptions<UserManagementOption> options)
+    public HomeController(ILogger<HomeController> logger, IOptions<UserManagementOption> options, IContext db)
     {
       _logger = logger;
+      this.db = db;
       option = options.Value ?? UserManagementOption.Detault;
-      
+
     }
 
     public IActionResult Index()
@@ -28,8 +32,14 @@ namespace ReheeCmfPackageTest.Controllers
     public async Task<IActionResult> TTT()
     {
       var c = new Requesta();
-      await c.TryAdding();
-      return View();
+      var result = await c.TryAdding(db);
+      var total = result.Sum(b => b.timeMs);
+      return Ok(new
+      {
+        total = total,
+        avg = total / result.Length,
+        result = result
+      });
     }
 
     public IActionResult Options()
@@ -51,23 +61,32 @@ namespace ReheeCmfPackageTest.Controllers
 
   public class Requesta : RequestBase
   {
-    public async Task TryAdding()
+
+    public async Task<checkResult[]> TryAdding(IContext db)
     {
       await Task.CompletedTask;
-      //for (var i = 0; i < int.MaxValue; i++)
-      //{
-      //  var dic = new Dictionary<string, string>();
-      //  var json = JsonConvert.SerializeObject(dic);
-      //  var start = DateTime.Now;
-      //  await Request(GetHttpClient(), HttpMethod.Post, "https://reheecmf.azurewebsites.net/Api/Data/EntityInput/-1", json);
-      //  var end = DateTime.Now;
-
-      //  Console.WriteLine($"line {i} processed {(end - start).TotalMilliseconds} total ms");
-      //}
+      var result = new List<checkResult>();
+      //var dbset = db.Create<>
+      for (var i = 0; i < 100; i++)
+      {
+        var dic = new Dictionary<string, string>();
+        var json = JsonConvert.SerializeObject(dic);
+        var start = DateTime.Now;
+        db.Create<EntityInput>(new EntityInput { CheckBox = "True" });
+        //await Request(GetHttpClient(), HttpMethod.Post, "https://reheecmf.azurewebsites.net/Api/Data/EntityInput/-1", json);
+        var end = DateTime.Now;
+        result.Add(new checkResult() { line = i, timeMs = (int)(end - start).TotalMilliseconds }); ;
+      }
+      return result.ToArray();
     }
     protected override HttpClient GetHttpClient(string name = null)
     {
       return new HttpClient();
     }
+  }
+  public class checkResult
+  {
+    public int line { get; set; }
+    public int timeMs { get; set; }
   }
 }
