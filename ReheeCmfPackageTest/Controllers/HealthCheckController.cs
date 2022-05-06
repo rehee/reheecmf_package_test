f2f06c07898ad86f7b89a7010d255a0ebc16a2f0
rehee <x3iii131@gmail.com>
2022-05-06 14:33:34 +0100

    add default for get current user

--- ReheeCmfPackageTest/Controllers/HealthCheckController.cs
+++ ReheeCmfPackageTest/Controllers/HealthCheckController.cs
@@ -1,23 +1,29 @@
 ﻿using Cruds;
+using JWT;
 using Microsoft.AspNetCore.Mvc;
 using Reflects;
+using ReheeCmf.Base.Entities;
+using ReheeCmf.JWT;
+using ReheeCmf.Server.Controllers;
+using ReheeCmfPackageTest.Data;
+using ReheeCmfPackageTest.Entities;
 
 namespace ReheeCmfPackageTest.Controllers
 {
-  public class HealthCheckController : Controller
+  public class HealthCheckController : ReheeCmfController
   {
     private readonly IContextFactory factory;
 
-    public HealthCheckController(IContextFactory factory)
+    private readonly ApplicationDbContext db2;
+
+    public HealthCheckController(IContextFactory factory, ApplicationDbContext db2, IJWTService jwt) : base(db2, jwt)
     {
       this.factory = factory;
+
+      this.db2 = db2;
     }
     public IActionResult Index()
     {
-      foreach (var t in ReflectPool.EntityMapping.Select(b => b.Key))
-      {
-        t.GetType().ThisType().GetMap();
-      }
       var result = factory.HealthCheckFunc();
       return Ok(result.Content);
     }
