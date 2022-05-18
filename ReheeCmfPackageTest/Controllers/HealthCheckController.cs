69739d7a60912c1b770982a51f6a7655e3b442c0
rehee <x3iii131@gmail.com>
2022-05-18 22:47:10 +0100

    update patch and clean cache

--- ReheeCmfPackageTest/Controllers/HealthCheckController.cs
+++ ReheeCmfPackageTest/Controllers/HealthCheckController.cs
@@ -3,6 +3,7 @@ using JWT;
 using Microsoft.AspNetCore.Mvc;
 using Reflects;
 using ReheeCmf.Base.Entities;
+using ReheeCmf.DBContext.Caches;
 using ReheeCmf.JWT;
 using ReheeCmf.Server.Controllers;
 using ReheeCmfPackageTest.Data;
@@ -15,16 +16,19 @@ namespace ReheeCmfPackageTest.Controllers
     private readonly IContextFactory factory;
 
     private readonly ApplicationDbContext db2;
+    private readonly ReheeCmfContextCache mc;
 
-    public HealthCheckController(IContextFactory factory, ApplicationDbContext db2, IJWTService jwt) : base(db2, jwt)
+    public HealthCheckController(IContextFactory factory, ApplicationDbContext db2, IJWTService jwt,ReheeCmfContextCache mc) : base(db2, jwt)
     {
       this.factory = factory;
 
       this.db2 = db2;
+      this.mc = mc;
     }
     public IActionResult Index()
     {
       var result = factory.HealthCheckFunc();
+      mc.CleanExpiredCache();
       return Ok(result.Content);
     }
   }
